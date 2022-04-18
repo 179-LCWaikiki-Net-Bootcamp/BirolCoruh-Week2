9bada7b3d0f69eec3612232436497f8ae10c35f4
Birol Coruh <65693604+birolcoruh26@users.noreply.github.com>
2022-04-19 00:54:46 +0300

    aaa

---
+++ StoreApi_Ef/DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    internal class IGenericDal
+    {
+        public interface IGenericDal<T> where T : class
+        {
+            void Insert(T item);
+            void Update(T item);
+            void Delete(T item);
+            List<T> GetAll();
+            T GetById(int id);
+        }
+    }
+}
