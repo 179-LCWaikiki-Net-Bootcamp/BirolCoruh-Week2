4abcc62093ec5ead2884521ca1f62798c2a7e697
Birol Coruh <65693604+birolcoruh26@users.noreply.github.com>
2022-04-20 23:56:46 +0300

    kkk

---
+++ StoreApi_Ef/BusinessLayer/Mapping/MapProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Dtos;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Mapping
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+            CreateMap<Product, ProductWithCategoryDto>();
+            CreateMap<Category, CategoryWithProductDto>();
+        }
+        
+    }
+}
